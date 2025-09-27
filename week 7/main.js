26717d39030f63a0575edd0968d7f9ce7fc5d93f
Sushmita-S06 <sushmitasallam@gmail.com>
2025-09-27 09:15:36 +0530

    Delete week 7/main.js

--- week 7/main.js
+++
@@ -1,9 +0,0 @@
-import { fetchBooks } from './books.js'; 
-import { renderBooks, setupAddToCartListeners, renderCart } from './ui.js'; 
- 
-document.addEventListener('DOMContentLoaded', async () => { 
-  const books = await fetchBooks(); 
-  renderBooks(books); 
-  setupAddToCartListeners(books); 
-  renderCart(); 
-});
